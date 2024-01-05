34ecafd1daa0f4c240023e6d6e565b3caf59aae8
Wenchuan <111623150+WenchuanLiliZhao@users.noreply.github.com>
2024-01-05 13:37:08 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css'
 
 import { HashRouter } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <HashRouter basename="/">
     <App />
   </HashRouter>
