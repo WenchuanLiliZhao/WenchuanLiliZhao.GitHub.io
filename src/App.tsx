2eb048c74aa503f7d6905ae63e686857eee45b49
Wenchuan <111623150+WenchuanLiliZhao@users.noreply.github.com>
2024-01-05 16:16:50 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <main className='App'>
       <Routes>
-        <Route index element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path='/about' element={<About />} />
       </Routes>
     </main>
