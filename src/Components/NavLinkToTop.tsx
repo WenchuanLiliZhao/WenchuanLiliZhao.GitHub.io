6907681dfb5f0326c4eee06e7bfd72940030b7d0
Wenchuan <111623150+WenchuanLiliZhao@users.noreply.github.com>
2024-01-05 17:15:55 +0800

    Update NavLinkToTop.tsx

--- src/Components/NavLinkToTop.tsx
+++ src/Components/NavLinkToTop.tsx
@@ -3,7 +3,6 @@ import { NavLink } from "react-router-dom"
 function NavLinkToTop(props: any) {
   const handleClick = () => {
     window.scroll(0, 0); // Scroll to the top of the page
-    document.querySelectorAll(".lili-window-content.scroll-y")[0].scrollTo({ top: 0, left: 0, behavior: 'auto' });
   };
 
   return (
